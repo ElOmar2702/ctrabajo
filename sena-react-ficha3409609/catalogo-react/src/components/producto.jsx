1edb794cd2d5c6dc8580874e37a9cce65cba8285
Omar Mayorga <omayorgacruz1@gmail.com>
2026-08-18 11:07:12 -0500

    first commit

---
+++ sena-react-ficha3409609/catalogo-react/src/components/producto.jsx
@@ -0,0 +1,12 @@
+import download from './download.jpg'
+
+export default function Producto() {
+  return (
+    <article className="producto">
+      <img src={download} alt="Producto" />
+      <h2>Labial mate</h2>
+      <p>Producto para catálogo de belleza.</p>
+      <strong>$18.000</strong>
+    </article>
+  )
+}
